d988860ecf410fea25fc87352c03d370597e2d19
Marin-B-git <mrnbilac@gmail.com>
2025-12-12 09:36:18 -0800

    Create KillCilantro.java

---
+++ KillCilantro.java
@@ -0,0 +1,21 @@
+public class KillCilantro {
+   public static void main(String[] args) {
+      
+      new String = "Cilantro";
+      new String cilantro = " ";
+      new String parsley = " ";
+      }
+      public static class KillCilantro {
+      if (String.substring = "Cilantro")
+      {
+      System.out.println("Parsley");
+      }
+      System.out.println("Cilantro")
+    }
+}
+ 
+
+
+
+
+   
